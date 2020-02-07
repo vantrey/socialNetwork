db2b25a1c613c521d8cf5244dde926a8d9e18211
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-07 17:43:22 +0300

    soc net start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Technologies/>
-            <Header/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href=''>Profile</a>
+                </div>
+                <div>
+                    <a href=''>Messages</a>
+                </div>
+                <div>
+                    <a href=''>News</a>
+                </div>
+                <div>
+                    <a href=''>Music</a>
+                </div>
+                <div>
+                    <a href=''>Settings</a>
+                </div>
+
+
+            </nav>
+            <div className='content'>
+                maim content
+            </div>
         </div>
     );
 }
