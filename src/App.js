02724746fc75e4b0a3fc8c5c8d215042d4fe1006
Ivan Tolkachev <vantrey@yandex.ru>
2020-02-08 13:22:28 +0300

    lesson 14 ccs modules

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href=''>Profile</a>
-                </div>
-                <div>
-                    <a href=''>Messages</a>
-                </div>
-                <div>
-                    <a href=''>News</a>
-                </div>
-                <div>
-                    <a href=''>Music</a>
-                </div>
-                <div>
-                    <a href=''>Settings</a>
-                </div>
-
-
-            </nav>
-            <div className='content'>
-                maim content
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5b0WN5OIEMhbg_WlCL2OwFimtuDGGtXzNnqq3mpT5P1nUN9Hc'>
+                </img>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>
+                        post1
+                    </div>
+                    <div className={s.item}>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
