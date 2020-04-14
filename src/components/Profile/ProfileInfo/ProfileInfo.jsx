df62865b78c44d2af44d96d09fdbd60c3550b5b2
Ivan Tolkachev <vantrey@yandex.ru>
2020-04-14 19:24:07 +0300

    59. complete. add description on profileInfo page from API complete

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader"
 import Contact from "./Contact/Contact"
+import yesImg from "../../../assets/images/yes-png-image.png"
+import noImg from "../../../assets/images/noImg.png"
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -16,19 +18,33 @@ const ProfileInfo = (props) => {
       </div>
 
       <div className={styles.description}>
-        <img src={props.profile.photos.large}/>
-        <span>
-          {props.profile.aboutMe}
-        </span>
-        <h3>Contacts:</h3>
-        <Contact contactName={props.profile.contacts.facebook}/>
-        <Contact contactName={props.profile.contacts.website}/>
-        <Contact contactName={props.profile.contacts.vk}/>
-        <Contact contactName={props.profile.contacts.twitter}/>
-        <Contact contactName={props.profile.contacts.instagram}/>
-        <Contact contactName={props.profile.contacts.youtube}/>
-        <Contact contactName={props.profile.contacts.github}/>
-        <Contact contactName={props.profile.contacts.mainLink}/>
+        <h1>{props.profile.fullName}</h1>
+        <img className={styles.photo} src={props.profile.photos.large}/>
+        <p>
+          "{props.profile.aboutMe}"
+        </p>
+        <div className={styles.contactsJobWrapp}>
+          <div className={styles.contacts}>
+            <h3>Contacts:</h3>
+            <Contact contactName={props.profile.contacts.facebook}/>
+            <Contact contactName={props.profile.contacts.website}/>
+            <Contact contactName={props.profile.contacts.vk}/>
+            <Contact contactName={props.profile.contacts.twitter}/>
+            <Contact contactName={props.profile.contacts.instagram}/>
+            <Contact contactName={props.profile.contacts.youtube}/>
+            <Contact contactName={props.profile.contacts.github}/>
+            <Contact contactName={props.profile.contacts.mainLink}/>
+          </div>
+          <div className={styles.job}>
+            <div className={styles.jobTitle}>
+              <h3>Looking for a jod: </h3>
+              {(props.profile.lookingForAJob &&
+                <img src={yesImg}></img>) ||
+              <img src={noImg}/>}
+            </div>
+            <span>{props.profile.lookingForAJobDescription}</span>
+          </div>
+        </div>
       </div>
 
     </div>
